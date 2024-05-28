800790bbc58397ab017e01afe718ced55086c1ad
Wathuomichael <wathuomichael@gmail.com>
2024-05-28 20:23:02 +0300

    fresh install changes

--- init.lua
+++ init.lua
@@ -53,18 +53,7 @@ vim.g.maplocalleader = ' '
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend('~/.config/nvim/lazy')
 
 -- [[ Configure plugins ]]
 -- NOTE: Here is where you install your plugins.
